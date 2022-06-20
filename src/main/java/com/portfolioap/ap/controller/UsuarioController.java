8312ff54510553ed6eb5e7e1ac35f017e2c4efc0
diego <dsruggeri@gmail.com>
2022-06-20 11:56:57 -0300

    prueba conexion con front

--- src/main/java/com/portfolioap/ap/controller/UsuarioController.java
+++ src/main/java/com/portfolioap/ap/controller/UsuarioController.java
@@ -5,6 +5,7 @@ import com.portfolioap.ap.model.Usuario;
 import com.portfolioap.ap.services.UsuarioService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/usuario")
+@CrossOrigin(origins = "http://localhost:4200")
 public class UsuarioController {
     
     private final UsuarioService usuarioService;
